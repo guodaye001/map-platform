383535c509ea6102c74659cad460ac0999e9887b
Guosw <Guosw@Guosw-PC>
2016-05-01 23:45:16 +0800

    sfdsfsdsdfsfsdfsdfsdfsdf

---
+++ src/main/java/com/map/platform/interfaces/dao/ILocusDao.java
@@ -0,0 +1,8 @@
+package com.map.platform.interfaces.dao;
+
+import com.map.platform.commons.dao.IBaseDao;
+import com.map.platform.interfaces.beans.LocusInfo;
+
+public interface ILocusDao extends IBaseDao<LocusInfo>{
+
+}
---
+++ src/main/java/com/map/platform/interfaces/service/ILocusService.java
@@ -0,0 +1,8 @@
+package com.map.platform.interfaces.service;
+
+import com.map.platform.commons.service.IBaseService;
+import com.map.platform.interfaces.beans.LocusInfo;
+
+public interface ILocusService extends IBaseService<LocusInfo>{
+	
+}
