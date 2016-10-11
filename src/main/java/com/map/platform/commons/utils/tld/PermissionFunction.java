b2edeb35b97ca259b117e8452502462e39e8362a
guosw <470657538@qq.com>
2016-10-11 17:50:38 +0800

    提交新增的类

---
+++ src/main/java/com/map/platform/commons/utils/tld/PermissionFunction.java
@@ -0,0 +1,47 @@
+package com.map.platform.commons.utils.tld;
+
+import java.util.List;
+
+import com.map.platform.commons.utils.Constants;
+import com.map.platform.commons.utils.SpringUtil;
+
+/**
+ * @author Guo
+ * 自定义标签权限许可标签tld
+ */
+public class PermissionFunction {
+	/**
+	 * 验证是否有权限
+	 * @param id
+	 * @return
+	 */
+	public static boolean check(String id){
+		boolean result = false;
+		if(Constants.PERMISSION_FLAG){
+			List<String> list = (List<String>)SpringUtil.getApplicationContext().get.get(Constants.SESSION_OPERATION_SRESOURCES);
+			result = list.contains(id);
+		}else{
+			result = true;
+		}
+		return result;
+	}
+	
+	/**
+	 * 格式化字符串日期
+	 * @param date
+	 * @return
+	 */
+	public static String format(String date){
+		String result = "";
+		if(date != null && !"".equals(date)){
+			if(date.length() == 8){
+				result = date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6, 8);
+			}else if(date.length() == 14){
+				result = date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6, 8) + " " + date.substring(8, 10) + ":" + date.substring(10, 12) + ":" + date.substring(12, 14);
+			}else{
+				result = date;
+			}
+		}
+		return result;
+	}
+}
